545edadb3faa067093db6009e616d7dd75667384
Abdul-Razak Adam <abadam@microsoft.com>
2020-07-30 12:19:00 -0700

    endpoints updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const request = require('request');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 var validUrl = require('valid-url');
+const { stringify } = require('querystring');
 
 
 async function main() {
@@ -17,33 +18,43 @@ async function main() {
   let app = express();
   app.use(cookieParser());
 
+  app.get('/clean', (req, res)=>{
+    var input = (req.query.input) ? req.query.input.toString() : ""
+    console.log({input})
+    if (input.indexOf("<at>tooMoo</at>") != -1){
+      input = input.split("<at>tooMoo</at>")[1]
+    }
+    input = input.trim()
+    res.send(input)
+  })
+
   app.get('/', async (req, res) => {
-    var url = (req.query.url).toString()
-    if (url.indexOf("tooMoo") != -1){
-      url = url.split("tooMoo")[1]
+    var url = (req.query.url) ? req.query.url.toString() : ""
+    console.log({url})
+    if (url.indexOf("<at>tooMoo</at>") != -1){
+      url = url.split("<at>tooMoo</at>")[1]
     }
     url = url.trim()
 
     if (validUrl.isHttpsUri(url)){
       ytdl.getInfo(url)
       .then(info=>{
-        res.send({title : info.videoDetails.title, url : url})        
+        if (info != null){
+          res.send({title : info.videoDetails.title, url : url})
+        }else{
+          console.log(`{info} is null`)
+          res.send({error: `${url} is not a valid url`})
+        }
       })
       .catch(err=>{
-        res.status(500).send(err);
+        console.log(err);
+        res.send(err)
       })
     }else {
-      res.status(500).send({error: `${url} is not a valid url`})
+      console.log({url})
+      res.send({error: `${url} is not a valid url`})
     }
-    
   });
-
-  function errorHandler (err, req, res, next) {
-    res.status(500)
-    res.render('error', { error: err })
-  }
-
-  app.use(errorHandler)
 
   // Create the HTTP server.
   let server = http.createServer(app);
